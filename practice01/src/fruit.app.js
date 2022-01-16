ff2955bf6480f466389bdb260ed85f0000a12dc1
Byungdon Yoon <byungdon92@gmail.com>
2022-01-17 00:47:08 +0900

    리팩터링 - 모듈 분리

--- practice01/src/fruit.app.js
+++ practice01/src/fruit.app.js
@@ -1,60 +1,9 @@
 import "dotenv/config";
-import FruitCache from "./cache/fruit.cache";
-import { getFruitsFromDB } from "./util/io.util";
-import readlineSync from "readline-sync";
-import comparePwd from "./util/crypt.util";
+import login from "./service/login.service";
+import acceptQuery from "./service/query.service";
 
 //* User's login
-const userid = readlineSync.question("Enter userid? ");
-const password = readlineSync.question("Enter password? ", {
-  hideEchoBack: true,
-  mask: "?",
-});
-if (await loginFailed(userid, password)) {
-  console.info("No such user.");
-  process.exit(0);
-}
-
-//* .db data cache
-const cache = new FruitCache();
-getFruitsFromDB().forEach((fruit) => cache.save(fruit));
+await login();
 
 //* Ready for user's query
-while (true) {
-  const query = readlineSync.question("> ");
-  translate(query);
-}
-
-async function loginFailed(userid, password) {
-  const useridMatch = process.env.USER_ID === userid;
-  const passwordMatch = await comparePwd(password, process.env.PASSWORD);
-  return useridMatch && !passwordMatch;
-}
-
-function translate(query) {
-  if (query.includes("find")) {
-    console.info(findFruit(query));
-  } else if (query.includes("delete")) {
-  } else if (query.trim() === "exit") {
-    console.info("See ya.");
-    process.exit(0);
-  } else if (query.trim() === "help") {
-    console.log("Find one : find [fruit]");
-    console.log("Find all : find --all");
-  } else {
-    console.log("Wrong grammer.");
-  }
-}
-
-function findFruit(query) {
-  const arr = query.trim().split(/\s+/);
-  if (arr.length !== 2) return "Wrong grammer.";
-  if (arr[1] === "--all") {
-    return cache
-      .findAll()
-      .map((fruit) => fruit.toString())
-      .join(",\n");
-  }
-  const fruit = cache.find(arr[1]);
-  return fruit === null ? "No such fruit." : fruit.toString();
-}
+acceptQuery();
